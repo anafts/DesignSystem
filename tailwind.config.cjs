c56d43b272c0bd8377019228f7b46d1ac4d6a38c
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-12 15:11:46 -0300

    created new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,9 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
+      'black': '#000',
+      "white": '#fff',
+      
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7C7C8A',
