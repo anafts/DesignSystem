61630a16d7fc9954910e92486166cbb1a491eb19
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-14 11:23:51 -0300

    publish storybook right url

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/IgniteLab-DesignSystem/'
+    }
+
+    return config
   }
 }
