2e42d987dee58cdfbc761f63125e7be72d852b03
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-14 17:50:23 -0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import './styles/global.css';
-
+import { SignIn } from './pages/SignIn';
 
 export function App() {
-
   return (
-
-    <>
-    <h1 className="font-bold text-2xl"> Hello world </h1>
-
-    <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300"> Enviar </button>
-
-    </>
+    <SignIn/>
   )
- 
 }
 
 
